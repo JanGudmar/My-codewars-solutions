ed29386baaa92831cf62f17cb8db5cbc0ed015f9
JanGudmar <>
2020-08-03 09:09:20 +0300

    Delete [7 kyu]List Filtering.js

--- [7 kyu]List Filtering.js
+++
@@ -1,7 +0,0 @@
-// https://www.codewars.com/kata/53dbd5315a3c69eed20002dd - kata description.
-
-function filter_list(l) {
-    return l.filter(function (areThereAnyNums) {
-        return typeof (areThereAnyNums) == "number"
-    })
-}
